05b6b2189f9a051a5191c1ef303a1199718ef7fc
jojo el fuente <jonathan.fuentes@outlook.fr>
2020-04-10 17:59:20 +0200

    Fuentes Jonathan

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -2,9 +2,27 @@
 public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
-
-        // TODO
-
-        return input;
+        int numberChar = 0;
+        int numberWords = 0;
+        StringBuilder newString = new StringBuilder();
+        String[] fullWords = input.split(" ");
+        for(String word:fullWords){
+            numberChar += word.length();
+            numberWords++;
+            if(numberWords > 1)
+                numberChar++;
+            if(numberChar <= columnNumber || (word.length() >= columnNumber && numberWords == 1)){
+                if(newString.length() == 0) {
+                    newString.append(word);
+                }
+                else
+                    newString.append(" ").append(word);
+            }else{
+                newString.append("\n").append(word);
+                numberChar = word.length();
+                numberWords = 1;
+            }
+        }
+        return newString.toString();
     }
 }
