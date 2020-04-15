2d73c142d6403b93ed58f68048a7868459db95cd
Peter <peterbalivet@gmail.com>
2020-04-16 00:44:09 +0200

    Peter BALIVET: all test OK

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -1,10 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
 
-        // TODO
-
-        return input;
+        String cutInput = "", tempStr = "";
+        long countSpace = 0;
+        List<String> words = new ArrayList<>(Arrays.asList(input.split("\\s+")));
+        words.add("");
+        int counter = 0;
+        while (words.get(counter) != "") {
+            if((tempStr + words.get(counter)).length() <= columnNumber || words.get(counter).length() >= columnNumber){
+                if((tempStr + words.get(counter)).length() > words.get(counter).length() && (tempStr + words.get(counter)).length() > columnNumber){
+                    cutInput += tempStr.substring(0,tempStr.length()-1) + '\n';
+                    tempStr = "";
+                }else{
+                    tempStr += words.get(counter)+' ';
+                    counter ++;
+                }
+            }else{
+                cutInput += tempStr.substring(0,tempStr.length()-1) + '\n';
+                tempStr = "";
+            }
+        }
+        cutInput = (tempStr.length() > 1) ? cutInput += tempStr.substring(0,tempStr.length()-1) :  cutInput;
+        return cutInput;
     }
 }
