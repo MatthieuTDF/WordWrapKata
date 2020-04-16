f656fb8a5fedfb2e32f7625caa3f4baf48f9808e
Maxime LALO <maxime.lalo.pro@gmail.com>
2020-04-16 18:43:24 +0200

    All tests passed

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -1,10 +1,22 @@
-
 public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
-
-        // TODO
-
-        return input;
+        String[] words = input.split(" ");
+        String line = "";
+        String finalText = "";
+        for (String word : words) {
+            if (line.length() == 0){
+                line = word;
+            }else{
+                if ( (word.length() + line.length()) < columnNumber){
+                    line += " " + word;
+                }else{
+                    finalText += line + "\n";
+                    line = word;
+                }
+            }
+        }
+        finalText += line;
+        return finalText;
     }
 }
