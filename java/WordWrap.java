cc74e38429e49640f260d274e1e63ee036b8aac4
Léo <Daviere-leo@hotmail.fr>
2020-04-16 12:57:03 +0200

    Test Ok

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -3,8 +3,28 @@ public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
 
-        // TODO
+        StringBuilder builder = new StringBuilder(input);
+        int pos;
 
-        return input;
+        for(int i = columnNumber;i<input.length();i+= columnNumber+1){
+            pos = i;
+                while(builder.charAt(i) != ' '){
+                  if(builder.charAt(i) != '\n' && i>=1)
+                      i--;
+                  else {
+                      while(pos < builder.length() && builder.charAt(pos) != ' ' )
+                          pos++;
+
+                      if(pos == builder.length())
+                          return builder.toString();
+
+                      i = pos;
+                      break;
+                  }
+                }
+            builder.setCharAt(i,'\n');
+        }
+        return builder.toString();
     }
+
 }
