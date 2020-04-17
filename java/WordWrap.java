e0d5c6238355f38c1ecfec9c7e50d2b85b864f8a
Shaadit <56261900+Shaadit@users.noreply.github.com>
2020-04-17 20:30:32 +0200

    Add files via upload

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -2,9 +2,50 @@
 public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
+        String substring;
+        int temp = 0;
+        StringBuilder final_input = new StringBuilder();
+        boolean stringEnd = false;
+        int columnNumber_temp = 0;
 
-        // TODO
+        String tab[] = input.split(" ");
+        if (tab.length == 1){
+            return input;
+        }
 
-        return input;
+        while(!stringEnd) {
+            try {
+
+                columnNumber_temp = columnNumber + temp;
+
+                substring = input.substring(temp, columnNumber_temp);
+
+                while (substring.charAt(substring.length()-1) != ' ' && substring.length() - 1 != 0) {
+                    columnNumber_temp--;
+                    substring = input.substring(temp, columnNumber_temp);
+                }
+
+                if (substring.length() - 1 == 0) {
+                    columnNumber_temp = columnNumber + temp;
+                    substring = input.substring(temp, columnNumber_temp);
+                    while (substring.charAt(substring.length() - 1) != ' ') {
+                        columnNumber_temp++;
+                        substring = input.substring(temp, columnNumber_temp);
+                    }
+                }
+
+                //System.out.println(substring.charAt(substring.length() - 1));
+
+                temp += substring.length() ;
+
+                final_input.append(substring).append('\n');
+
+            } catch (Exception e) {
+                stringEnd = true;
+            }
+        }
+
+        return final_input.toString();
     }
 }
+
