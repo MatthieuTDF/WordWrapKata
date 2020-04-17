be819ca3502596aa563de54f3381ff3e112f87fe
hania <ohania@hotmail.fr>
2020-04-17 19:23:16 +0200

    Hania Ourabah

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -3,8 +3,40 @@ public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
 
-        // TODO
+        String[] tab = input.split(" ");
+        String phrase = "";
+        String finalphrase ="";
 
-        return input;
+        if (tab.length == 1){
+            return tab[0];
+        }
+
+        for(int i = 0; i < tab.length; i++){
+            if(tab[i].length() >= columnNumber) {
+                if(!phrase.isEmpty()){
+                    finalphrase += phrase.trim() + "\n";
+                    phrase = "";
+                }
+                finalphrase += tab[i] + "\n";
+            } else if ((tab[i].length() + phrase.length() + 1) > columnNumber){
+                finalphrase += phrase + "\n";
+                phrase = tab[i];
+            } else {
+                phrase += " " + tab[i];
+                phrase = phrase.trim();
+            }
+
+        }
+
+        if (phrase.length() != 0){
+             finalphrase += phrase;
+        }
+
+        if(finalphrase.endsWith("\n")) {
+            finalphrase = finalphrase.substring(0, finalphrase.length()-1);
+
+        }
+
+        return finalphrase;
     }
 }
