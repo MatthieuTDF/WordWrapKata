2db72ba40e938ce19283e0e6909d788a4f23a881
SonJu <jclinchamps@gmail.com>
2020-04-18 16:11:31 +0200

    last version, full nuke

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class WordWrap {
 
@@ -5,49 +6,71 @@ public class WordWrap {
 
         // TODO
 
-        //first try, full fail -_-, je ne sais pas pourquoi
         //put input in an array so we can modify and search a specified character
         char[] stringArray = input.toCharArray();
 
         int length = input.length();
         int i;
         int position;
+        char space = ' ';
 
         //go through the input until the last jump
-        for (i = 0, position = 0; i < length; position = 0) {
+        for (i = 0, position = 0; i <= length; i+=columnNumber,position = 0) {
 
             ///in order to avoid out of range
-            if (i + columnNumber > length - columnNumber) {
+           if (i + columnNumber >= length) {
 
                 break;
 
             }
-            System.out.println(stringArray);
+
             ///check if the last character to be displayed is a space
-            if (stringArray[i + columnNumber] == ' ') {
+            if (stringArray[i + columnNumber -1] == ' ') {
 
                 ///if it's a space, then we can change it to a \n
-                stringArray[i + columnNumber] = '\n';
+                stringArray[i + columnNumber -1] = '\n';
 
                 ///jump to the next line
-                i = i + columnNumber;
+                //i = i + columnNumber;
 
-            } else {
+                ///case of a word not finished at columnNumber
+            } else if (stringArray[i + columnNumber] == ' '){
 
-                ///if it's a char then we don't want to cut a word so we go back and increment as long as it's a letter
-                while (stringArray[i + columnNumber + position] != ' ') {
+                ///case of a word that ends at columnNumber
+                stringArray[i + columnNumber] = '\n';
+                //i = i + columnNumber;
+
+            }else if((input.substring(0, i + columnNumber)).indexOf((space)) != -1){
+
+                ///As long as the word isn't finished, we want to look for the previous space
+                while(stringArray[i + columnNumber + position] != ' ') {
+
+                    position--;
+
+                }
+                stringArray[i + columnNumber + position] = '\n';
+                //i = i + columnNumber + position;
+
+                ///looking for the next space if there is one
+            }else if((input.substring(i + columnNumber, length)).indexOf((space)) != -1){
+
+                while(stringArray[i + columnNumber + position] != ' ') {
 
                     position++;
+
                 }
-
-                ///go to the nearest space and make it a \n
                 stringArray[i + columnNumber + position] = '\n';
-                i = i + columnNumber + position;
+                //i = i + columnNumber + position;
 
-            }
+                ///no more space => print as it is
+            }else break;
+
+            input = new String(stringArray);
         }
 
-        input = stringArray.toString();
+        //input = new String(stringArray);
+       // input = Arrays.toString(stringArray);
+        //input = stringArray.toString();
         return input;
     }
 }
