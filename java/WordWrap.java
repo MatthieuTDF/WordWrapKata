ab6c798e4dc6bc64fa4102e83c4c699d9acb2361
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2020-04-18 17:22:19 +0200

    Kata fini et fonctionnel. Les tests n'ont pas pu être exécutés, ils tournent en boucle sans donner de résultat.

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -2,9 +2,60 @@
 public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
+        //int strLength = ;
 
-        // TODO
+        char[] charInput = input.toCharArray();
+        String[] words = new String[input.length()];
+        String word = "";
+        int j = 0;
+        for(int i = 0;i < charInput.length;i++)
+        {
+            if(!Character.isWhitespace(charInput[i]))
+            {
+                word += charInput[i];
+                if(i == charInput.length-1)
+                {
+                    words[j] = word;
+                    //System.out.println(word);
+                    j++;
+                    word = "";
+                }
+            }
+            else
+            {
+                words[j] = word;
+                //System.out.println(word);
+                j++;
+                word = "";
+            }
+        }
+        String line = "";
+        input = "";
+        for(int i = 0;i < words.length ;i++)
+        {
+            try
+            {
+                //System.out.println(words[i]);
 
+                int strLength = line.length() + words[i].length();
+                if (strLength < columnNumber) {
+                    line += words[i] + " ";
+                }
+                else
+                {
+                    //System.out.println(line);
+                    input += line + "\n";
+                    i--;
+                    line = "";
+                }
+            }
+            catch (NullPointerException e)
+            {
+                //System.out.println(line);
+                input += line;
+                break;
+            }
+        }
         return input;
     }
 }
