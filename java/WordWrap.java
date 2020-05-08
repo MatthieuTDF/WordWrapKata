ede42e72f33535101d95a53ac8eccd7604f6581c
Dherissart <dhugot-44@outlook.fr>
2020-05-08 22:11:55 +0200

    worldwrap-Dherissart Hugo

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -2,9 +2,35 @@
 public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
+        int i = 0;
+        int count;
+        int taille = input.length();
 
-        // TODO
+        StringBuffer buffer = new StringBuffer(input);
 
+        for (i = 0, count = 0; i < taille ; count = 0 , i++) {
+
+            if (i < taille - columnNumber){
+
+                if (input.charAt(i + columnNumber ) == ' ') {
+
+                    buffer.setCharAt(i + columnNumber , '\n');
+                    input = buffer.toString();
+                    i = i + columnNumber;
+
+                } else {
+
+                    while (input.charAt(i + columnNumber - count) != ' '){
+                        count++;
+                    }
+
+                    buffer.setCharAt(i + columnNumber - count, '\n');
+                    input = buffer.toString();
+                    i = i + columnNumber - count;
+
+                }
+            }
+        }
         return input;
     }
 }
